917e3e65fa340817b7573b283645e63cc8b335fd
Tuti Wulandari <tutiwulandari150@gmail.com>
2021-02-27 15:41:08 +0700

    learn kotlin-navigation

---
+++ app/src/main/java/com/example/learnkotlin/form/FormFragment.kt
@@ -0,0 +1,31 @@
+package com.example.learnkotlin.form
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.learnkotlin.R
+
+
+class FormFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_form, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = FormFragment()
+    }
+}
---
+++ app/src/main/java/com/example/learnkotlin/presentations/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.learnkotlin.presentations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.learnkotlin.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
